f846c10f3f6324184c8aaa55444dc9c17e8d3c45
Romain Day <romain.ludovic.day@gmail.com>
2023-02-27 17:24:26 +0100

    feat(index): test + code

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,34 @@
-import { describe, expect, test } from '@jest/globals';
-import ChiffreRomain from './index';
-describe('Chiffre Romain', () => {
-	test('Chiffre Romain 1 = I', () => {
-		expect(ChiffreRomain(1)).toBe('I');
-	});
+import { describe, expect, test } from "@jest/globals";
+import ChiffreRomain from "./index";
+describe("Chiffre Romain", () => {
+  test("Chiffre Romain 1 = I", () => {
+    expect(ChiffreRomain(1)).toBe("I");
+  });
+  test("Chiffre Romain 3 = III", () => {
+    expect(ChiffreRomain(3)).toBe("III");
+  });
+  test("Chiffre Romain 4 = IV", () => {
+    expect(ChiffreRomain(4)).toBe("IV");
+  });
+  test("Chiffre Romain 5 = V", () => {
+    expect(ChiffreRomain(5)).toBe("V");
+  });
+  test("Chiffre Romain 8 = VIII", () => {
+    expect(ChiffreRomain(8)).toBe("VIII");
+  });
+  test("Chiffre Romain 9 = IX", () => {
+    expect(ChiffreRomain(9)).toBe("IX");
+  });
+  test("Chiffre Romain 10 = X", () => {
+    expect(ChiffreRomain(10)).toBe("X");
+  });
+  test("Chiffre Romain 18 = XVIII", () => {
+    expect(ChiffreRomain(18)).toBe("XVIII");
+  });
+  test("Chiffre Romain 99 = XVIII", () => {
+    expect(ChiffreRomain(18)).toBe("XVIII");
+  });
+  test("Chiffre Romain 88 = LXXXVIII", () => {
+    expect(ChiffreRomain(88)).toBe("LXXXVIII");
+  });
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,42 @@
 export default function ChiffreRomain(n: number) {
-	if (n === 1) {
-		console.log('I');
-	}
+  let result: string = "";
+  //   while (n > 0) {
+  //     if (n < 4) {
+  //       result += "I";
+  //     }
+  //     if (n === 4) {
+  //       result = "IV";
+  //     }
+  //     if (n < 9) {
+  //       result == "V" + "I" * ;
+  //     }
+  // 	n--
+  //   }
+  while (n >= 50) {
+    n -= 50;
+    result += "L";
+  }
+  while (n >= 10) {
+    n -= 10;
+    result += "X";
+  }
+  while (n === 9) {
+    n -= 9;
+    result += "IX";
+  }
+  while (n >= 5) {
+    n -= 5;
+    result += "V";
+  }
+  while (n === 4) {
+    n -= 4;
+    result += "IV";
+  }
+
+  while (n > 0) {
+    n--;
+    result += "I";
+  }
+
+  return result;
 }
-ChiffreRomain(1);
