3823cabbd98ce2a6feea83611c6ac3380faaaa2d
Romain Day <romain.ludovic.day@gmail.com>
2023-02-27 17:31:14 +0100

    feat(index): update code

--- index.ts
+++ index.ts
@@ -1,17 +1,6 @@
 export default function ChiffreRomain(n: number) {
   let result: string = "";
-  //   while (n > 0) {
-  //     if (n < 4) {
-  //       result += "I";
-  //     }
-  //     if (n === 4) {
-  //       result = "IV";
-  //     }
-  //     if (n < 9) {
-  //       result == "V" + "I" * ;
-  //     }
-  // 	n--
-  //   }
+
   while (n >= 50) {
     n -= 50;
     result += "L";
@@ -20,15 +9,15 @@ export default function ChiffreRomain(n: number) {
     n -= 10;
     result += "X";
   }
-  while (n === 9) {
+  if (n === 9) {
     n -= 9;
     result += "IX";
   }
-  while (n >= 5) {
+  if (n >= 5) {
     n -= 5;
     result += "V";
   }
-  while (n === 4) {
+  if (n === 4) {
     n -= 4;
     result += "IV";
   }
